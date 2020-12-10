1371088d3c6ae61cf2513e01fa86cc1669670364
Eli <72849929+lumas13@users.noreply.github.com>
2020-12-10 14:48:54 +0800

    Rotate/DPlat

---
+++ Assets/DPlatController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DPlatController : MonoBehaviour
+{
+    private float countDownTimer = 5f;
+    public GameObject dPLAT;
+    
+
+    bool timerIsRunning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerIsRunning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning == true)
+        {
+            countDownTimer -= Time.deltaTime;
+            Debug.Log((int)countDownTimer);
+            if (countDownTimer <= 0)
+            {
+                Destroy(dPLAT);
+            }
+
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            timerIsRunning = true;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,56 +4,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float moveSpeed = 5f;
-    private float jumpPower = 5f;
+    private float moveSpeed = 6f;
+    private float jumpPower = 6f;
 
     public Rigidbody playerRB;
 
-    bool isOnGround = true;
+    private bool isOnGround;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOnGround = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movements WASD
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
-        else if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
-        else if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0, -90, 0);
         }
-        else if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0, 90, 0);
         }
 
+        //Jumping, Single
         if (Input.GetKey(KeyCode.Space) && isOnGround)
         {
-            isOnGround = false;
-            playerRB.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            PlayerJump();
+        }
+
+        if (transform.position.y <= -1)
+        {
+            Debug.Log("Lose!");
         }
     }
 
+    void PlayerJump()
+    {
+        isOnGround = false;
+        playerRB.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Platforms"))
         {
             isOnGround = true;
-            Debug.Log("touch");
+            //Debug.Log("touch");
+        }
+
+        if (collision.gameObject.CompareTag("DissPlat"))
+        {
+
         }
     }
 }
